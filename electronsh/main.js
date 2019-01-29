49cae81ad2009ac237849398f7502f319bf8fd08
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2019-01-29 16:25:04 +0800

    electronsh 有个共有配置，使新窗口能有自设图标等。新窗口通信通道

--- electronsh/main.js
+++ electronsh/main.js
@@ -27,22 +27,24 @@ function window_open(url, conf, o) {
 	if(conf) {
 		if(conf.max_)
 			w.maximize()
-		if(!conf.menubar_)
+		if(!conf.menubar_) {
+			console.log('!conf.menubar_')
 			w.setMenu(null)
+		}
 	}
 	return w
 }
 
 let mainWindow
+let conf_ = {menubar_:true}
 function createWindow () {
-	let conf = {menubar_:true}
 	let app = process.env.ELECTRONSH_APP
 	if(app) {
 		let s = z$('加载“' + app + 'app.conf.zs”')
 		console.log(s)
-		conf = eval(s)
+		conf_ = eval(s)
 		
-		if(conf.sock_) {
+		if(conf_.sock_) {
 			const net = require('net')
 			const fs = require('fs')
 			let server = net.createServer((socket) => {
@@ -57,18 +59,18 @@ function createWindow () {
 					console.log("意外 " + e)
 				})
 			})
-			if (fs.existsSync(conf.sock_))
-				fs.unlinkSync(conf.sock_)
-			server.listen(conf.sock_, () => console.log('开始监听'))
+			if (fs.existsSync(conf_.sock_))
+				fs.unlinkSync(conf_.sock_)
+			server.listen(conf_.sock_, () => console.log('开始监听'))
 		}
 	}
-	mainWindow = window_open('file://' + __dirname + '/index.html', conf)
+	mainWindow = window_open('file://' + __dirname + '/index.html', conf_)
 	mainWindow.on('closed', function () {
 		mainWindow = null
-		if(conf.sock_) {
+		if(conf_.sock_) {
 			const fs = require('fs')
-			if (fs.existsSync(conf.sock_))
-				fs.unlinkSync(conf.sock_)
+			if (fs.existsSync(conf_.sock_))
+				fs.unlinkSync(conf_.sock_)
 		}
 	})
 }
@@ -84,12 +86,32 @@ app.on('activate', function () {
 })
 
 ipc_.on('window_open', function(event, url, conf, o) {
-	window_open(url, conf, o)
+	let conf1 = {}
+	Object.assign(conf1, conf_)
+	conf1.max_ = false
+	Object.assign(conf1, conf)
+	window_open(url, conf1, o)
 })
 ipc_.on('window_min', function(event, wid) {
 	BrowserWindow.fromId(wid).minimize()
 })
+ipc_.on('conf', function(event, arg) {
+	let ret
+	switch(arg) {
+	case 'zs_path':
+		ret = conf_.path_ ? conf_.path_ : __dirname + '/bin/zhscript2/'
+		break
+	}
+	console.log(arg + ': ' + ret)
+	event.returnValue = ret
+})
 
 ipc_.on('index.html', function(event, arg1, arg2, arg3, arg4) {
+	//console.log('main index.html\n\t' + arg1 + '\n\t' + arg2 + '\n\t' + arg3 + '\n\t' + arg4)
 	mainWindow.webContents.send('index.html', arg1, arg2, arg3, arg4)
+})
+
+ipc_.on('o_', function(event, arg1, arg2, arg3, arg4, arg5, arg6, arg7) {
+	//console.log('main o_\n\t' + arg1 + '\n\t' + arg2 + '\n\t' + arg3 + '\n\t' + arg4 + '\n\t' + arg5 + '\n\t' + arg6 + '\n\t' + arg7)
+	BrowserWindow.fromId(arg1).webContents.send('i_', arg3, arg4, arg5, arg6, arg7)
 })
